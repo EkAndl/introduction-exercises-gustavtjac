bf1db3a33024ecf63c10c7aaab3b08a6353fad08
Gustav Jacobsen <gustavtorup@gmail.com>
2026-01-29 12:47:49 +0100

    opgaver lavet færdigt

--- 3._Objects.js
+++ 3._Objects.js
@@ -7,11 +7,17 @@ const greetings = { message: "Hello, earthling! I bring peace." };
 
 // Log the message 
 
-console.log(greetings.message)
+console.log(greetings.message);
 
 // --------------------------------------
 // Exercise 2 - Defining an object.
 
+
+
+const gustavoObject = {
+name:"Gustav Torup Jacobsen",
+age: 22
+};
 
 // Create an object that has your name and age. 
 
@@ -19,17 +25,26 @@ console.log(greetings.message)
 // --------------------------------------
 // Exercise 3 - Add a key-value pair
 
-const stackOverflow = {};
+const stackOverflow = {
+key : "value"
+};
 
 // make a rule called isAllowed and let the value be true
+
+const isAllowed = true;
 
 // --------------------------------------
 // Exercise 4 - Remove a property
 
 const thisSong = { description: "The best song in the world." };
 
+delete thisSong.description;
+
+thisSong.about = "just a tribute";
+
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
+console.log(thisSong);
 
 // --------------------------------------
 
